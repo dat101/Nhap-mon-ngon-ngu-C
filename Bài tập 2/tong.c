0731d9febb330e9190b0d2214605e25f535329d9
dat101 <72543241+dat101@users.noreply.github.com>
2020-12-03 14:46:03 +0700

    Create tong.c

---
+++ Bài tập 2/tong.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+//Nhap hai so a, b tu ban phim
+//hien thi tong a va b 
+int main() {
+	int a, b;
+  printf("Nhap a= ");
+  scanf("%d", &a);
+  printf("Nhap b= ");
+  scanf("%d", &b);
+  printf("a + b = %d", a + b);
+  
+  return 0;
+}
