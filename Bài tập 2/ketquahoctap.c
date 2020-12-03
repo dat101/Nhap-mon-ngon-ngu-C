545cd0da7d8e9985c3ca6f0e64a9d3b23cfd75ec
dat101 <72543241+dat101@users.noreply.github.com>
2020-12-03 16:35:42 +0700

    Create ketquahoctap.c

---
+++ Bài tập 2/ketquahoctap.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+    //Nhap diem van va diem toan tu ban phim
+int main(void) {
+	int v, t, dtb;
+printf("Nhap v= ");
+scanf("%d", &v);
+printf("Nhap t= ");
+scanf("%d", &t);
+    //Tong diem trung binh van va toan
+printf("v + t = %d", v + t );
+    //Hien thi diem van, toan, diem trung binh
+printf("Diemvan(v)\tDiemtoan(t)\tDiemtrungbinh(dtb)\n");
+printf("%d\t%d\t%d", t, v, dtb);
+
+return 0;
+}
