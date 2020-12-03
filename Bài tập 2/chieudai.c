f9e2b11c4bc6cdf8eec3ee688bfdbc88cb52af04
dat101 <72543241+dat101@users.noreply.github.com>
2020-12-03 16:47:14 +0700

    Update chieudai.c

--- Bài tập 2/chieudai.c
+++ Bài tập 2/chieudai.c
@@ -4,7 +4,7 @@ int main(void) {
  double l;
  printf("nhap do dai can chuyen doi l = ");
  scanf("%lf", &l);
- printf("\nDo dai chuyen sang don vi xentimet la: %.f", l*100);
- printf("\nDo dai chuyen sang don vi milimet la: %.f", l*1000);
+ printf("Met(m)\tXentimet(cm)\tMilimet(mm)\n");
+ printf("%f\t%.f\t%.f", l, l*100, l*1000);
   return 0;
 }
