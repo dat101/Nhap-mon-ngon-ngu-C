e4922e785da8149126ff8eb8ae2c8ad9927d4e4e
dat101 <72543241+dat101@users.noreply.github.com>
2020-12-10 16:06:32 +0700

    Create sosanh.c

---
+++ Bài tập 3/sosanh.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main() {
+// nhap hai so a va b tu ban phim
+    int a, b ;
+    if (a, b > 0){
+// hien thi ra man hinh a > b    	
+    	printf ("a > b", a, b);
+    	scanf ("%d", &a, &b);
+    }else if(a, b = 0) {
+// hien thi ra man hinh a = b  	
+	    printf ("a = b", a, b);
+	    scanf ("%d", &a, &b);
+	}else if(a,b < 0) {
+// hien thi ra man hinh a < b		
+	    printf ("a < b", a, b);
+	    scanf ("%d", &a, &b);
+	}else {
+	    printf ("%d", a, b);
+	}			
+
+    getchar();
+    return 0;
+}    
+    	
