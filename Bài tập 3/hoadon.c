a6a17394ecf6a56d82e1563c88ec213d672044bc
dat101 <72543241+dat101@users.noreply.github.com>
2020-12-16 12:59:35 +0700

    Update hoadon.c

--- Bài tập 3/hoadon.c
+++ Bài tập 3/hoadon.c
@@ -3,18 +3,17 @@
 int main() {
 // nhap bien dongia, soluong,thanhtien tu ban phim
 	double dongia, soluong, thanhtien;
-printf("Nhap don gia mat hang =");
-scanf("%fl", &dongia);
 printf("Nhap so luong mat hang =");
 scanf("%fl", &soluong);
+printf("Nhap don gia mat hang =");
+scanf("%fl", &dongia);
     if (soluong >=5 ) {
 // tinh thanh tien, giam gia 25% khi mua tren 5 mat hang
-thanhtien = ( dongia*soluong )*0.75;
+thanhtien=(dongia*soluong)*0.75;
   	}else {
- printf("Don gia\tso luong\tthanh tien\n");
- printf("%.f\t%.f\t%.f", dongia, soluong, thanhtien);
-}
- 
+printf("so tien can phai tra la =");
+scanf("%fl", &thanhtien);
+ }
   getchar();
   return 0;
  } 		
