435a5a463ef2d67ee09d30115150390186a45dee
dat101 <72543241+dat101@users.noreply.github.com>
2020-12-17 14:35:43 +0700

    Update and rename variable.c to hoadon.c

--- Bài tập 1/hoadon.c
+++ Bài tập 1/hoadon.c
@@ -3,14 +3,14 @@
 int main() {
 // nhap bien dongia, soluong,thanhtien tu ban phim
 	float dongia, soluong, thanhtien;
-printf("Nhap so luong mat hang =");
+printf("Nhap so luong mat hang = ");
 scanf("%fl", &soluong);
-printf("Nhap don gia mat hang =");
+printf("Nhap don gia mat hang = ");
 scanf("%fl", &dongia);
 // tinh thanh tien, giam gia 50% khi mua tren 5 mat hang va don gia tren 500
     if (soluong >=5 && dongia >=500 ) {
 thanhtien=(dongia*soluong)*0.5;
-printf("%.f", thanhtien);
+printf("So tien can phai tra la = %.f", thanhtien);
   	}else {
 printf("%.f", thanhtien);
 
