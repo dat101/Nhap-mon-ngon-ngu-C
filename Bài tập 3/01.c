361cee4fe0fc270acd2e6bf41de98bdae38ab62e
dat101 <72543241+dat101@users.noreply.github.com>
2021-01-07 16:20:06 +0700

    Update 01.c

--- Bài tập 3/01.c
+++ Bài tập 3/01.c
@@ -4,9 +4,9 @@ int main(){
 	int  a, b;
 	
 	printf("Bang cuu chuong \n\n\n");
-	printf("Bang cuu chuong 2:\n 2 x 1 = 2\n 2 x 2 = 4\n 2 x 3 = 6\n");
-	printf("Bang cuu chuong 3:\n 3 x 1 = 3\n 3 x 2 = 6\n 3 x 3 = 9\n");
-	printf("Bang cuu chuong 4:\n 4 x 1 = 4\n 4 x 2 = 8\n 4 x 3 = 12\n");
+	printf("Bang cuu chuong 2:\n\n 2 x 1 = 2\n 2 x 2 = 4\n 2 x 3 = 6\n\n");
+	printf("Bang cuu chuong 3:\n\n 3 x 1 = 3\n 3 x 2 = 6\n 3 x 3 = 9\n\n");
+	printf("Bang cuu chuong 4:\n\n 4 x 1 = 4\n 4 x 2 = 8\n 4 x 3 = 12\n\n");
 	
 	return 0;
 }	
