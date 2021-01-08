a89e49228e711e1e76a9d245cd150d33b8790555
dat101 <72543241+dat101@users.noreply.github.com>
2021-01-08 11:46:19 +0700

    Update kt01.c

--- Bài tập 3/kt01.c
+++ Bài tập 3/kt01.c
@@ -1,12 +1,17 @@
 #include <stdio.h>
  
 int main(){
-	int  a;
+	int i, j;
 	
-	printf("Bang cuu chuong \n\n\n");
-	printf("Bang cuu chuong 2:\n\n 2 x 1 = 2\n 2 x 2 = 4\n 2 x 3 = 6\n\n");
-	printf("Bang cuu chuong 3:\n\n 3 x 1 = 3\n 3 x 2 = 6\n 3 x 3 = 9\n\n");
-	printf("Bang cuu chuong 4:\n\n 4 x 1 = 4\n 4 x 2 = 8\n 4 x 3 = 12\n\n");
-	
-	return 0;
-}	
+       printf("-----Bang cuu chuong-----\n\n ");
+	 
+    for(i = 2; i <= 4;i++) {
+		
+    for(j = 1; j <= 3; j++){
+	    
+       printf("\t%d x %d = %d\n", i, j, i*j);
+       
+   }
+   }
+    return 0;
+}	   
