4fe95014dad3e76539344083eefaeeb4b1288063
dat101 <72543241+dat101@users.noreply.github.com>
2021-01-08 10:44:25 +0700

    Update kt03.c

--- Bài tập 3/kt03.c
+++ Bài tập 3/kt03.c
@@ -12,3 +12,20 @@ if (s == 5 || s == 11 || s == 15)
 
  return 0;
 } 
+------------------------------------------
+#include <stdio.h>
+
+int main(){
+
+int s = 1; 
+while( s <= 20 ) {
+
+s++;
+if (s == 5 || s == 11 || s == 15)
+   continue;
+
+   printf("%d ",s);
+}
+
+ return 0;
+} 
