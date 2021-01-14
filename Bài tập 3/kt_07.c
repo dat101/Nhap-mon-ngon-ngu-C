2a5dee2171ab065f86fba92cc19f39be9bb55775
dat101 <72543241+dat101@users.noreply.github.com>
2021-01-14 21:45:35 +0700

    Update kt_07.c

--- Bài tập 3/kt_07.c
+++ Bài tập 3/kt_07.c
@@ -5,33 +5,33 @@
 int main ()
 {   clrscr ();
     float a,b,c;
-    printf("Nhap a:");
-    scanf("%f",&a);
-    printf("Nhap b:");
-    scanf("%f",&b);
-    printf("Nhap c:");
-    scanf("%f",&c);
-    if ( a+b > c || b+c>a || c+a>b)
+       printf("Nhap a:");
+       scanf("%f",&a);
+       printf("Nhap b:");
+       scanf("%f",&b);
+       printf("Nhap c:");
+       scanf("%f",&c);
+   if ( a+b > c || b+c>a || c+a>b)
     {
-        if ( a*a + b*b == c*c || b*b + c*c == a*a || c*c + a*a == b*b)
+   if ( a*a + b*b == c*c || b*b + c*c == a*a || c*c + a*a == b*b)
+    {
+       printf("\n 3 so ban vua nhap la do dai 3 canh cua 1 tam giac vuong");
+          }
+   else
         {
-            printf("\n 3 so ban vua nhap la do dai 3 canh cua 1 tam giac vuong");
-        }
-        else
+   if( a== b==c)
         {
-            if( a== b==c)
-            {
-                printf("\n 3 so ban vua nhap la do dai 3 canh cua 1 tam giac deu");
-            }
-            else
-            {
-            if(a*a > b*b + c*c || b*b > a*a + c*c || c*c > a*a + b*b)
-            {
-                printf("\n 3 so ban vua nhap la do dai 3 canh cua 1 tam giac tu");
-            }
-            else
-            {
-                printf("\n 3 so ban vua nhap la do dai 3 canh cua 1 tam giac nhon");
+       printf("\n 3 so ban vua nhap la do dai 3 canh cua 1 tam giac deu");
+          }
+    else
+        {
+    if(a*a > b*b + c*c || b*b > a*a + c*c || c*c > a*a + b*b)
+        {
+       printf("\n 3 so ban vua nhap la do dai 3 canh cua 1 tam giac tu");
+           }
+     else
+        {
+       printf("\n 3 so ban vua nhap la do dai 3 canh cua 1 tam giac nhon");
             }
             }
         }
@@ -41,6 +41,6 @@ int main ()
     printf("\n Ba so ban vua nhap ko phai la do \
         dai 3 canh cua 1 tam giac");
     }
-    getch ();
-    return 0;
+    
+   return 0;
 }
