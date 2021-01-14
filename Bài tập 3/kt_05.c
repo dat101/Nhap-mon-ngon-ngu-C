097ba6ada12d90d10989fa7fdece518ecbfe30e3
dat101 <72543241+dat101@users.noreply.github.com>
2021-01-14 20:54:53 +0700

    Create kt_05.c

---
+++ Bài tập 3/kt_05.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+ 
+void main()
+{
+    int i, n;
+    int tongle = 0, tongchan = 0;
+ 
+    printf("Nhap so nguyen: ");
+    scanf("%d", &n);
+    for (i = 1; i <= n; i++)
+    {
+        if (i % 2 == 0)
+            tongchan = tongchan + i;
+        else
+            tongle = tongle + i;
+    }
+    printf("Tong cua cac so le la: %d\n", tongle);
+    printf("Tong cua cac so chan la: %d\n", tongchan);
+}
