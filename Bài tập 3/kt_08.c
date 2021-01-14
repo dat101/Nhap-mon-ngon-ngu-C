473292eaced9076874fb000fe167714f67bf83cf
dat101 <72543241+dat101@users.noreply.github.com>
2021-01-14 21:47:23 +0700

    Create kt_08.c

---
+++ Bài tập 3/kt_08.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+int main() {
+	float a, b;
+	   printf("Nhap a: ");
+	   scanf("%f", &a);
+	   printf("Nhap b: ");
+	   scanf("%f", &b);
+
+ if(a*b > 0 ) 
+	printf("\nCung dau", a, b);
+ else 
+        printf("\nKhong cung dau", a, b);
+
+	 
+  return 0;
+}
