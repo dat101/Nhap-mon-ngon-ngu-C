a9e3830d7efce62493b179986ad482f2b771fedb
dat101 <72543241+dat101@users.noreply.github.com>
2021-01-21 17:00:33 +0700

    Create mang_06.c

---
+++ Bài tập 4/mang_06.c
@@ -0,0 +1,38 @@
+#include<stdio.h>
+ int main() {
+ 	int n,i;
+ 	int m;
+ 	
+ 	printf("Nhap n = ");
+ 	scanf("%d", &n);
+ 	
+ 	int a[n];
+ 	
+ 	for (i = 0; i < n; i++) {
+ 		printf("a[%d]: ", i);
+ 		scanf("%d", &a[i]);
+ 	
+	 printf("\n");
+	 	
+	 }	     
+		printf("Mang a: ");
+	for (i = 0; i < n; i++) {	
+	 	printf("%d ",a[i]);
+}     
+     printf("\n");  
+	    
+	printf("Nhap m = ");
+	scanf("%d", &m);
+	
+	for (i = 0; i < n; i++) {
+   if (a[i] == m) 
+
+	printf("\nCo ton tai");
+		
+   else 
+   printf("\nKhong ton tai");
+   break;
+
+}		
+	return 0;
+	}
