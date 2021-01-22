891c7a5afc30b06aa69ede96dc184a6cbffe5ad7
dat101 <72543241+dat101@users.noreply.github.com>
2021-01-22 14:31:48 +0700

    Update mang_06.c

--- Bài tập 4/mang_06.c
+++ Bài tập 4/mang_06.c
@@ -1,38 +1,63 @@
+// Khoi khai bao
 #include<stdio.h>
- int main() {
- 	int n,i;
- 	int m;
- 	
- 	printf("Nhap n = ");
- 	scanf("%d", &n);
- 	
- 	int a[n];
- 	
- 	for (i = 0; i < n; i++) {
- 		printf("a[%d]: ", i);
+int n; 
+int i;
+
+void Nhap_pt_mang(int a[]);
+void Hien_thi_pt_mang(int a[]);
+void Hien_thi_tong_chan(int a[]);
+void Dem_chan_le_0(int a[]);
+
+ //khoi ham main
+int main(){
+  
+  printf("Nhap n = ");
+  scanf("%d", &n);
+  int a[n];
+  Nhap_pt_mang(a);
+  Hien_thi_pt_mang(a);
+  Hien_thi_tong_chan(a);
+  Dem_chan_le_0(a);
+  return 0;
+}
+
+// khoi dinh nghia ham
+void Nhap_pt_mang(int a[]) {
+	
+  	for (i = 0; i < n; i++) {
+ 		printf("Nhap phan tu thu %d: ", i + 1);
  		scanf("%d", &a[i]);
- 	
-	 printf("\n");
 	 	
-	 }	     
-		printf("Mang a: ");
+}
+}	 
+	
+void Hien_thi_pt_mang(int a[]) {     
+ 		printf("\nphan tu mang a: ");
 	for (i = 0; i < n; i++) {	
 	 	printf("%d ",a[i]);
-}     
-     printf("\n");  
-	    
-	printf("Nhap m = ");
-	scanf("%d", &m);
-	
-	for (i = 0; i < n; i++) {
-   if (a[i] == m) 
-
-	printf("\nSo phan tu co gia tri la ");
-		
-   else 
-   printf("\nKhong ton tai m trong mang a");
-   break;
-
-}		
-	return 0;
-	}
+} 
+}
+    
+void Hien_thi_tong_chan(int a[]) {
+        printf("\nHien thi tong chan: ");
+    int tong = 0;    
+    for (i = 0; i < n; i++) {
+    	tong = tong + a[i];
+    	if (a[i] % 2 == 0)
+		printf("%d", a[i]);
+}
+}
+    void Dem_chan_le_0(int a[]) {
+    	
+    int dem_chan = 0, dem_le = 0;
+    
+    for (i = 0; i < n; i++) {
+    if(a[i] % 2 == 0)
+        dem_chan = dem_chan + i;
+    else 
+	dem_le = dem_le + i;
+}
+    printf("\nCac so le: %d", dem_le);
+    printf("\nCac so chan: %d", dem_chan);
+  
+}
