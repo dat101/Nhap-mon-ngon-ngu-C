7025c3239abaaeafc7e84c6c5fd6fe746047e268
dat101 <72543241+dat101@users.noreply.github.com>
2021-01-22 15:03:04 +0700

    Update ham_03.c

--- Bài tập 5/ham_03.c
+++ Bài tập 5/ham_03.c
@@ -1,9 +1,16 @@
-//Khoi khai bao
+// Khoi khai bao
 #include<stdio.h>
-int pt_mang(int n);
+int n; 
+int i;
+
+void Nhap_pt_mang(int a[]);
+void Hien_thi_pt_mang(int a[]);
+void Hien_thi_tong_chan(int a[]);
+void Dem_chan_le_0(int a[]);
+
  //khoi ham main
 int main(){
-  int n;
+  
   printf("Nhap n = ");
   scanf("%d", &n);
   int a[n];
@@ -15,23 +22,34 @@ int main(){
 }
 
 // khoi dinh nghia ham
-  int pt_mang(int n) {
-  	for (int i = 0; i < n; i++) {
+void Nhap_pt_mang(int a[]) {
+	
+  	for (i = 0; i < n; i++) {
  		printf("Nhap phan tu thu %d: ", i + 1);
  		scanf("%d", &a[i]);
 	 	
-	 }	     
-		printf("phan tu mang a: ");
-	for (int i = 0; i < n; i++) {	
-	 	printf("%d",a[i]);
-}     
+}
+}	 
+	
+void Hien_thi_pt_mang(int a[]) {     
+ 		printf("\nphan tu mang a: ");
+	for (i = 0; i < n; i++) {	
+	 	printf("%d ",a[i]);
+} 
+}
     
-        printf("Hien thi tong chan: ");
-    for (int i = 0; i < n; i++) {
+void Hien_thi_tong_chan(int a[]) {
+        printf("\nHien thi tong chan: ");    
+    for (i = 0; i < n; i++) {
+    	int tong = 0;
     	tong = tong + a[i];
-    	if (a[i]%2 == 0)
+    	if (a[i] % 2 == 0)
 		printf("%d", a[i]);
 }
+}
+
+void Dem_chan_le_0(int a[]) {
+    	
     int dem_chan = 0, dem_le = 0;
     
     for (i = 0; i < n; i++) {
@@ -40,8 +58,7 @@ int main(){
     else 
 	dem_le = dem_le + i;
 }
-    printf("Cac so le: %d\n", dem_le);
-    printf("Cac so chan: %d", dem_chan);
-    
-    return 0;
-}    
+    printf("\nCac so chan: %d", dem_chan);
+    printf("\nCac so le: %d", dem_le);
+  
+}
