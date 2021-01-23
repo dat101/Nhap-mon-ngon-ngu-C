084b93a2d8f180fe57a86777b06f16c697f2c50b
dat101 <72543241+dat101@users.noreply.github.com>
2021-01-23 09:01:59 +0700

    Create TH_03.c

---
+++ Bài tập 5/TH_03.c
@@ -0,0 +1,26 @@
+int main() {
+ 	
+ 	printf("Nhap a = ");
+    scanf("%d", &a);
+	printf("Nhap b = ");
+    scanf("%d", &b);
+    printf("Nhap c = ");
+    scanf("%d", &c);
+    
+    
+    if(a > b && a > c) 
+    printf("So lon nhat la a");
+    
+    else if(b > a && b > c)
+	printf("So lon nhat b");
+	
+	else 
+	printf("So lon nhat la c");      
+
+    return 0;
+}    
+   int so_lon_nhat(int a, int b, int c) {
+   	
+   	
+   	return 0;
+}   	
