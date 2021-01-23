2b5c6b5cc2b9cae063f487d9a7937a8c932312c0
dat101 <72543241+dat101@users.noreply.github.com>
2021-01-23 08:32:47 +0700

    Create TH_02.c

---
+++ Bài tập 5/TH_02.c
@@ -0,0 +1,15 @@
+#include <stdio.h> 
+main() {
+	int n;
+	int S = 0;
+	
+	printf("Nhap n: ");
+	scanf("%d", &n);
+	
+	for(int i = 1; i <= n; i++) {
+		S = S + i;
+}
+    printf("\nTong la: %d ", S);
+	
+	return 0;
+}				
