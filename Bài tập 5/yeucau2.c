34cb480ceaf93c8f3b1b1ce314e002033e796696
dat101 <72543241+dat101@users.noreply.github.com>
2021-03-20 14:23:32 +0700

    Update yeucau2.c

--- Bài tập 5/yeucau2.c
+++ Bài tập 5/yeucau2.c
@@ -1,11 +1,8 @@
 #include <stdio.h>
-#include <math.h>
-#define Pi 3.14159
-
 int main() {
 
     int r;
-    float chuvi, dientich;
+    float chuvi, dientich, Pi = 3.14159;
 
         printf("Nhap ban kinh r: ");
         scanf("%d", &r);
