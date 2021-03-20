c2973c001a3a65998d96aa1566c66f87bcbd2439
dat101 <72543241+dat101@users.noreply.github.com>
2021-03-20 14:09:33 +0700

    Create yeucau3.c

---
+++ Bài tập 5/yeucau3.c
@@ -0,0 +1,23 @@
+#include <stdio.h> 
+ int boi_chung_nho_nhat(int a, int b);
+ 
+ int main(){
+    int a,b,bc;
+    printf("Nhap a: ");
+    scanf("%d",&a);
+    printf("Nhap b: ");
+    scanf("%d",&b);
+ 
+ for (bc=a;bc<=a*b;bc++){
+        if (bc%a==0 && bc%b==0){
+            printf("BCNN(%d,%d)= %d\n",a,b,bc);
+            break;
+        }
+    }
+    return 0;
+}
+
+ int boi_chung_nho_nhat(int a, int b) {
+ 	
+ 	return 0;
+ }
