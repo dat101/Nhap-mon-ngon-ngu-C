e6b09b6771ce2a3b1b0e4997d95b26258b6866e4
dat101 <72543241+dat101@users.noreply.github.com>
2021-03-20 13:49:06 +0700

    Create yeucau1.c

---
+++ Bài tập 5/yeucau1.c
@@ -0,0 +1,15 @@
+#include <stdio.h> 
+int main() {
+	int n;
+	int S = 0;
+	
+	printf("Nhap n: ");
+	scanf("%d", &n);
+	
+	for(int i = 1; i <= n; i++) {
+		S+= i*i;
+	}
+	printf("\nTong la: %d ", S);
+	
+	return 0;
+}			
